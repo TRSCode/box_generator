7ad8391a0627ac9c0be90c0c570b415e0dd7dc42
Tony Schock <tony.schock@gmail.com>
2023-04-15 17:11:08 -0600

    added clear input after submission on form by adding setColor in handler and value {color} on input

--- client/src/components/FormBox.jsx
+++ client/src/components/FormBox.jsx
@@ -3,9 +3,12 @@ import React, {useState} from 'react'
 const FormBox = (props) => {
     const { colorArray, setColorArray } = props;
     const [ color, setColor ]= useState('');
+    // const [inputValue, setInputValue] = useState("#000000")
     const handleSubmit = (e) => {
         e.preventDefault();
         setColorArray([...colorArray,color]);
+        setColor("");
+        // setInputValue("#000000");
     };
 
 
@@ -14,11 +17,11 @@ const FormBox = (props) => {
         <div>
             <form onSubmit = {handleSubmit} className="form-control">
                 <label htmlFor="Color" className="mx-1" >Type a Color</label>
-                <input type="text" name="color" onChange={(e) => setColor(e.target.value)} className="mx-1"/>
+                <input type="text" name="color" value={color} onChange={(e) => setColor(e.target.value)}  className="mx-1"/>
                 <input type="submit" value="Add" className="btn btn-success mx-1"/>
             </form>
         </div>
     )
 }
-
+// value={inputValue}
 export default FormBox
